9d5e5ef246aee64b7811accf1a1c16b7db2405f4
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-20 11:49:33 +0200

    Added OrdersPage

---
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/View/OrdersPage/UserOrdersPageReturn.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PPMobile.View.OrdersPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserOrdersPageReturn : ContentPage
+    {
+        public UserOrdersPageReturn()
+        {
+            InitializeComponent();
+        }
+    }
+}
