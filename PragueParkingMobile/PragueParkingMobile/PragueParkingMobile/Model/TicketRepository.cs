912f36c1ba3796a86ef161b2e32295f69ada2ef1
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-20 15:22:58 +0200

    Added support for binding with UserOrdersPageActive

---
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/Model/TicketRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PPMobile.Model
+{
+    public static class TicketRepository
+    {
+        static TicketRepository()
+        {
+            if (TicketList == null)
+            {
+                TicketList = new List<Ticket>
+                {
+                    new Ticket
+                    {
+                        RegNo = "ABC 123",
+                        ParkingSpotNo = 53,
+                        TimeCreated = "2020-05-20"
+                    },
+                    new Ticket
+                    {
+                        RegNo = "EFG 456",
+                        ParkingSpotNo = 33,
+                        TimeCreated = "2020-05-16"
+                    },
+                    new Ticket
+                    {
+                        RegNo = "HIJ 789",
+                        ParkingSpotNo = 13,
+                        TimeCreated = "2020-05-14"
+                    }
+                };
+            }
+        }
+
+        public static List<Ticket> TicketList { get; set; }
+    }
+}
---
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/ViewModel/UserOrdersPageVM.cs
@@ -0,0 +1,29 @@
+﻿using PPMobile.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace PPMobile.ViewModel
+{
+    public class UserOrdersPageVM : BaseViewModel
+    {
+        private ObservableCollection<Ticket> _ticketList;
+
+        public ObservableCollection<Ticket> TicketList
+        {
+            get => _ticketList;
+            set
+            {
+                _ticketList = value;
+                OnPropertyChanged("TicketList");
+            }
+        }
+
+        public UserOrdersPageVM()
+        {
+            TicketList = new ObservableCollection<Ticket>(TicketRepository.TicketList);
+        }
+    }
+}
