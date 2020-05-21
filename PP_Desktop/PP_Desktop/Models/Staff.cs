9b49c9692953dc1bee19bbca1980cb5f2434b12b
Mikael <m.klaening@gmail.com>
2020-05-21 10:47:00 +0200

    Model for Staff

---
+++ PP_Desktop/PP_Desktop/Models/Staff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Desktop.Models
+{
+    public class Staff
+    {
+        public int ID { get; set; }
+        public int PID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StaffAddress { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string BankAccount { get; set; }
+        public string ICE { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public int DepartmentsID { get; set; }
+    }
+}
