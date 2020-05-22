3efc125dc36fd66e89ba69c03a70d89626ddde08
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-22 11:33:20 +0200

    Added Navigation between UserMainPage and OrderPage and added another vm to ViewModelLocator

--- PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/Utility/ViewModelLocator.cs
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/Utility/ViewModelLocator.cs
@@ -2,11 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace PPMobile.Utility
 {
     public static class ViewModelLocator
     {
-        public static UserOrdersPageVM UserOrdersPageVM { get; set; } = new UserOrdersPageVM();  
+        public static UserOrdersPageVM UserOrdersPageVM { get; set; } = new UserOrdersPageVM();
+        public static UserMainPageVM UserMainPageVM { get; set; } = new UserMainPageVM(Navigation);
+
+        public static INavigation Navigation { get; set; }
     }
 }
