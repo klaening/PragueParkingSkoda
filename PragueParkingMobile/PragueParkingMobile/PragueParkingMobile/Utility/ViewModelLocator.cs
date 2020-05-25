e87ca52479e401d3f8baf5e3e24e3fc96c5e3d80
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-25 08:37:18 +0200

    Working with binding on UserOrdersPage

--- PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/Utility/ViewModelLocator.cs
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/Utility/ViewModelLocator.cs
@@ -8,7 +8,7 @@ namespace PPMobile.Utility
 {
     public static class ViewModelLocator
     {
-        public static UserOrdersPageVM UserOrdersPageVM { get; set; } = new UserOrdersPageVM();
+        public static UserOrdersPageVM UserOrdersPageVM { get; set; } = new UserOrdersPageVM(Navigation);
         public static UserMainPageVM UserMainPageVM { get; set; } = new UserMainPageVM(Navigation);
 
         public static INavigation Navigation { get; set; }
--- PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/ViewModel/UserOrdersPageVM.cs
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/ViewModel/UserOrdersPageVM.cs
@@ -1,15 +1,39 @@
 ﻿using PPMobile.Model;
+using PPMobile.View;
+using PPMobile.View.OrdersPage;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace PPMobile.ViewModel
 {
     public class UserOrdersPageVM : BaseViewModel
     {
+        public INavigation Navigation { get; set; }
+
         private ObservableCollection<Ticket> _ticketList;
+
+        private Ticket _selectedTicket;
+        public Ticket SelectedTicket
+        {
+            get { return _selectedTicket; }
+            set
+            {
+                if(_selectedTicket != value)
+                {
+                    _selectedTicket = value;
+                    HandleSelectedItem();
+                }
+            }
+        }
+
+        private void HandleSelectedItem()
+        {
+        }
 
         public ObservableCollection<Ticket> TicketList
         {
@@ -21,8 +45,9 @@ namespace PPMobile.ViewModel
             }
         }
 
-        public UserOrdersPageVM()
+        public UserOrdersPageVM(INavigation navigation)
         {
+            this.Navigation = navigation;
             TicketList = new ObservableCollection<Ticket>(TicketRepository.TicketList);
         }
     }
