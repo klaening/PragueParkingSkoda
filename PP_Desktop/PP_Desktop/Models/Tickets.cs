4b41993756c1247bccf747cf8952cbe1e44e4e2f
AlexanderLeafet <alexander.lovdahl@live.se>
2020-05-25 16:16:35 +0200

    Worked with list for tickets, not finnished

---
+++ PP_Desktop/PP_Desktop/Models/Tickets.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Desktop.Models
+{
+    public class Tickets
+    {
+        public int ID { get; set; }
+        public string RegNo { get; set; }
+        public string RetrievalCode { get; set; }
+        public string PhoneNo { get; set; }
+        public string PID { get; set; }
+        public decimal EstimatedParkingTime { get; set; }
+        public string Comment { get; set; }
+        public int ParkingSpotsID { get; set; }
+        public int VehicleTypesID { get; set; }
+
+        public static ObservableCollection<Tickets> TicketsList()
+        {
+            ObservableCollection<Tickets> ticket = new ObservableCollection<Tickets>()
+            {
+                new Tickets()
+                {
+                    ID = 1,
+                    RegNo = "ABC 123",
+                    RetrievalCode = "Parked",
+
+                },
+                new Tickets()
+                {
+                    ID = 2,
+                    RegNo = "DEF 456",
+                    RetrievalCode = "Recieved",
+
+                },
+
+                new Tickets()
+                {
+                    ID = 1,
+                    RegNo = "GHJ 789",
+                    RetrievalCode = "Parked",
+
+                }
+
+            };
+
+            return ticket;
+        }
+    }
+}
---
+++ PP_Desktop/PP_Desktop/ViewModels/TicketsMainPage_VM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PP_Desktop.Models;
+
+namespace PP_Desktop.ViewModels
+{
+    public class TicketsMainPage_VM
+    {
+        private ObservableCollection<Tickets> tickets_;
+        public ObservableCollection<Tickets> Ticketss
+        {
+            get
+            {
+                return tickets_;
+            }
+            set
+            {
+                tickets_ = value;
+            }
+        }
+
+        public TicketsMainPage_VM()
+        {
+            tickets_ = new ObservableCollection<Tickets>(Tickets.TicketsList());
+        }
+            
+        
+    }
+}
