18cf9b7ced8f52cebe0e9ca2a64a5f7468f03e6d
AndyCarlsson <andreas95carlsson@gmail.com>
2020-05-26 16:09:11 +0200

    Updated API call

--- PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/View/OrdersPage/UserOrdersPageActive.xaml.cs
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/View/OrdersPage/UserOrdersPageActive.xaml.cs
@@ -1,10 +1,12 @@
-﻿using PPMobile.Model;
+﻿using Newtonsoft.Json;
+using PPMobile.Model;
 using PPMobile.Utility;
 using PPMobile.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +21,8 @@ namespace PPMobile.View.OrdersPage
         public UserOrdersPageActive()
         {
             InitializeComponent();
-            BindingContext = ViewModelLocator.UserOrdersPageVM;
+            GetAllTickets();
+            //BindingContext = ViewModelLocator.UserOrdersPageVM;
         }
 
         private void ViewCell_Tapped(object sender, EventArgs e)
@@ -27,5 +30,22 @@ namespace PPMobile.View.OrdersPage
             Navigation.PushAsync(new AcceptOrderPage());
         }
         
+        private void GetAllTickets()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri("http://10.0.2.2:59893/api/");
+            string path = "tickets/";
+
+            var response = client.GetAsync(path);
+
+            var statusCode = response.Result;
+            string result = statusCode.Content.ReadAsStringAsync().Result;
+
+            var dbTickets = JsonConvert.DeserializeObject<ObservableCollection<Tickets>>(result);
+
+            TicketActiveList.ItemsSource = dbTickets;
+        }
+
     }
 }
