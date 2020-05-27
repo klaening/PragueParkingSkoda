b7386c9feac9109a3b2741372007d1594f4b7077
Mikael <m.klaening@gmail.com>
2020-05-27 11:32:18 +0200

    Get Request for Departments and disabled SSL

---
+++ WebAPI_PragueParking/WebAPI_PragueParking_Domain/Service/DepartmentsService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI_PragueParking_Domain.Models;
+using WebAPI_PragueParking_Domain.Repository;
+
+namespace WebAPI_PragueParking_Domain.Service
+{
+    public class DepartmentsService : IDepartmentsService
+    {
+        private readonly IDepartmentsRepository _departmentsRepository;
+
+        public DepartmentsService(IDepartmentsRepository departmentsRepository)
+        {
+            _departmentsRepository = departmentsRepository;
+        }
+
+        public async Task<IEnumerable<Departments>> GetDepartments()
+        {
+            return await _departmentsRepository.GetDepartments();
+        }
+
+        public async Task<Departments> GetDepartment(int id)
+        {
+            return await _departmentsRepository.GetDepartment(id);
+        }
+    }
+}
