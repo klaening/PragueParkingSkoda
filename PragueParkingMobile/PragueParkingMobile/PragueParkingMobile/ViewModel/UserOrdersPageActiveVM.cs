dd0d7f1e1955bfb9a84e29686ecfbce24cd77b18
AndyCarlsson <andreas95carlsson@gmail.com>
2020-06-01 11:03:39 +0200

    Remove old code from UserOrdersPages VM

--- PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/ViewModel/UserOrdersPageActiveVM.cs
+++ PragueParkingMobile/PragueParkingMobile/PragueParkingMobile/ViewModel/UserOrdersPageActiveVM.cs
@@ -12,32 +12,6 @@ namespace PPMobile.ViewModel
 {
     public class UserOrdersPageActiveVM : BaseViewModel
     {
-        private ObservableCollection<TicketInfoView> _ticketList;
-        public ObservableCollection<TicketInfoView> TicketList
-        {
-            get => _ticketList;
-            set
-            {
-                _ticketList = value;
-                
-                OnPropertyChanged("TicketList");
-            }
-        }
-        public UserOrdersPageActiveVM()
-        {
-            TicketList = new ObservableCollection<TicketInfoView>();
-
-            var path = "ticketinfoview";
-
-            var response = Services.GetRequest(path);
-            
-            var tempList = JsonConvert.DeserializeObject<ObservableCollection<TicketInfoView>>(response);
-
-            foreach (var ticket in tempList)
-            {
-                if (ticket.StatusName == "Return Accepted")
-                    TicketList.Add(ticket);
-            }
-        }
+        
     }
 }
