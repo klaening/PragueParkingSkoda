4490d3eada1fac23ad4548f2773bf20dfa8ab6d0
Mikael <m.klaening@gmail.com>
2020-06-02 15:47:06 +0200

    Post Request for Ticket through Stored Procedure

---
+++ WebAPI_PragueParking/WebAPI_PragueParking/Controllers/TicketsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_PragueParking_Domain.Models;
+using WebAPI_PragueParking_Domain.Service;
+
+namespace WebAPI_PragueParking.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketsController : Controller
+    {
+        private readonly ITicketsService _ticketsService;
+
+        public TicketsController(ITicketsService ticketsService)
+        {
+            _ticketsService = ticketsService;
+        }
+
+        [HttpPost("{staffID}")]
+        public async Task<IActionResult> Add([FromBody] Tickets ticket, int staffID)
+        {
+            return Ok(await _ticketsService.AddTicket(ticket, staffID));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _ticketsService.GetTickets());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _ticketsService.GetTicket(id));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] Tickets ticket)
+        {
+            return Ok(await _ticketsService.UpdateTicket(ticket));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _ticketsService.DeleteTicket(id));
+        }
+    }
+}
---
+++ WebAPI_PragueParking/WebAPI_PragueParking_Domain/Repository/ITicketsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI_PragueParking_Domain.Models;
+
+namespace WebAPI_PragueParking_Domain.Repository
+{
+    public interface ITicketsRepository
+    {
+        Task<bool> AddTicket(Tickets ticket, int staffID);
+        Task<IEnumerable<Tickets>> GetTickets();
+        Task<Tickets> GetTicket(int id);
+        Task<bool> UpdateTicket(Tickets ticket);
+        Task<bool> DeleteTicket(int id);
+    }
+}
